3ab7c70b392d6fa363c53c123ad38b65fc391b44
Lucas Travagin <lucas_travagin1112@hotmail.com>
2019-09-17 01:34:10 -0300

    Criando o arquivo de configuracao

---
+++ admin/config/config_upload.php
@@ -0,0 +1,8 @@
+<?php 
+    $config_upload['verifica_extensao'] = true;
+    $config_upload['extensoes'] = array('.gif','.jpeg','.png','.jpg','.bmp');
+    $config_upload['verifica_tamanho'] = true;
+    $config_upload['tamanho'] = 2097152; //2mb
+    $config_upload['caminho_absoluto'] = '../fotos';
+    $config_upload['renomeia'] = false;
+?>
